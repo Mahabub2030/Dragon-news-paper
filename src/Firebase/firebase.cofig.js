f204fc712faabbe3346f385ed6858a89ac8fe014
mahabub <mahabubalom72091@gmail.com>
2024-04-04 22:00:39 +0300

    adding

---
+++ src/Firebase/firebase.cofig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCypFDKXxWZ-DpQozk5qeKPL8oTGb7o9P4",
+  authDomain: "dragon-new-auth-85898.firebaseapp.com",
+  projectId: "dragon-new-auth-85898",
+  storageBucket: "dragon-new-auth-85898.appspot.com",
+  messagingSenderId: "514484673870",
+  appId: "1:514484673870:web:04e57f4ba248e9cb8b521f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/Routs/Route.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Leout/Root";
+
+const route = createBrowserRouter([
+        {
+                path:'/',
+                element:<Root></Root>,
+                children:[
+                        {
+                                path:'/',
+                                element:
+                        }
+                ]
+        }
+])
